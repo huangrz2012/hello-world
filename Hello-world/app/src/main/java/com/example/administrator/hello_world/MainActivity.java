2fd6616654b4cd3f479447b89e3ab5238f808246
huangrongzhao <huangrongzhao@xinhuotech.com>
2017-12-05 17:02:31 +0800

    部分工具方法，6.0的权限问题没做处理

--- Hello-world/app/src/main/java/com/example/administrator/hello_world/MainActivity.java
+++ Hello-world/app/src/main/java/com/example/administrator/hello_world/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.administrator.hello_world;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.administrator.hello_world.Utils.Utils;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.text_view);
+        textView.setText(Utils.getDeviceIMEI(this));
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Utils.call(MainActivity.this, "18720994193");
+            }
+        });
     }
 }
---
+++ Hello-world/app/src/main/java/com/example/administrator/hello_world/Utils/Utils.java
@@ -0,0 +1,244 @@
+package com.example.administrator.hello_world.Utils;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.telephony.TelephonyManager;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+/**
+ * Created by huangrongzhao on 2017/12/5 0005.
+ */
+
+public class Utils {
+
+    /**
+     * dp转px
+     */
+    public static int dp2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
+     * px转dp
+     */
+    public static int px2dp(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+    /**
+     * sp转px
+     */
+    public static int sp2px(Context context, float spValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
+
+    /**
+     * px转sp
+     */
+    public static int px2sp(Context context, float pxValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (pxValue / fontScale + 0.5f);
+    }
+
+    // 该方法存在于TypedValue
+
+    /**
+     * 各种单位转换
+     */
+    public static float applyDimension(int unit, float value, DisplayMetrics metrics) {
+        switch (unit) {
+            case TypedValue.COMPLEX_UNIT_PX:
+                return value;
+            case TypedValue.COMPLEX_UNIT_DIP:
+                return value * metrics.density;
+            case TypedValue.COMPLEX_UNIT_SP:
+                return value * metrics.scaledDensity;
+            case TypedValue.COMPLEX_UNIT_PT:
+                return value * metrics.xdpi * (1.0f / 72);
+            case TypedValue.COMPLEX_UNIT_IN:
+                return value * metrics.xdpi;
+            case TypedValue.COMPLEX_UNIT_MM:
+                return value * metrics.xdpi * (1.0f / 25.4f);
+        }
+        return 0;
+    }
+
+    /**
+     * 在onCreate()即可获取View的宽高
+     */
+    public static int[] getViewMeasure(View view) {
+        int widthMeasureSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+        int heightMeasureSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+        view.measure(widthMeasureSpec, heightMeasureSpec);
+        return new int[]{view.getMeasuredWidth(), view.getMeasuredHeight()};
+    }
+
+    // 通知父布局，占用的宽，高；
+
+    /**
+     * ListView中提前测量View尺寸，如headerView
+     */
+    private void measureView(View view) {
+        ViewGroup.LayoutParams p = view.getLayoutParams();
+        if (p == null) {
+            p = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT);
+        }
+        //第一个参数spec =0,mode是UNSPECIFIED(未指定)，父元素不对子元素施加任何束缚,第二个参数，是外边距和内边距
+        int width = ViewGroup.getChildMeasureSpec(0, 0, p.width);
+        int height;
+        int tempHeight = p.height;
+        if (tempHeight > 0) {
+            height = View.MeasureSpec.makeMeasureSpec(tempHeight, View.MeasureSpec.EXACTLY);
+        } else {
+            height = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+        }
+        view.measure(width, height);
+    }
+
+    /**
+     * 判断设备是否是手机
+     */
+    public static boolean isPhone(Context context) {
+        TelephonyManager telephony = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        return telephony.getPhoneType() != TelephonyManager.PHONE_TYPE_NONE;
+    }
+
+    /**
+     * 获取当前设备的IMIE，需与上面的isPhone一起使用
+     */
+    public static String getDeviceIMEI(Context context) {
+        String deviceId;
+        if (isPhone(context)) {
+            TelephonyManager telephony = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+            deviceId = telephony.getDeviceId();
+        } else {
+            deviceId = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
+        }
+        return deviceId;
+    }
+
+    // 需添加权限<uses-permission android:name="android.permission.READ_PHONE_STATE"/>
+
+    /**
+     * 获取手机状态信息
+     * <p>
+     * 返回如下
+     * DeviceId(IMEI) = 99000311726612
+     * DeviceSoftwareVersion = 00
+     * Line1Number =
+     * NetworkCountryIso = cn
+     * NetworkOperator = 46003
+     * NetworkOperatorName = 中国电信
+     * NetworkType = 6
+     * honeType = 2
+     * SimCountryIso = cn
+     * SimOperator = 46003
+     * SimOperatorName = 中国电信
+     * SimSerialNumber = 89860315045710604022
+     * SimState = 5
+     * SubscriberId(IMSI) = 460030419724900
+     * VoiceMailNumber = *86
+     */
+    public static String getPhoneStatus(Context context) {
+        TelephonyManager tm = (TelephonyManager) context
+                .getSystemService(Context.TELEPHONY_SERVICE);
+        String str = "";
+        str += "DeviceId(IMEI) = " + tm.getDeviceId() + "\n";
+        str += "DeviceSoftwareVersion = " + tm.getDeviceSoftwareVersion() + "\n";
+        str += "Line1Number = " + tm.getLine1Number() + "\n";
+        str += "NetworkCountryIso = " + tm.getNetworkCountryIso() + "\n";
+        str += "NetworkOperator = " + tm.getNetworkOperator() + "\n";
+        str += "NetworkOperatorName = " + tm.getNetworkOperatorName() + "\n";
+        str += "NetworkType = " + tm.getNetworkType() + "\n";
+        str += "honeType = " + tm.getPhoneType() + "\n";
+        str += "SimCountryIso = " + tm.getSimCountryIso() + "\n";
+        str += "SimOperator = " + tm.getSimOperator() + "\n";
+        str += "SimOperatorName = " + tm.getSimOperatorName() + "\n";
+        str += "SimSerialNumber = " + tm.getSimSerialNumber() + "\n";
+        str += "SimState = " + tm.getSimState() + "\n";
+        str += "SubscriberId(IMSI) = " + tm.getSubscriberId() + "\n";
+        str += "VoiceMailNumber = " + tm.getVoiceMailNumber() + "\n";
+        return str;
+    }
+
+    /**
+     * 是否有SD卡
+     */
+    public static boolean haveSDCard() {
+        return android.os.Environment.getExternalStorageState().equals(android.os.Environment.MEDIA_MOUNTED);
+    }
+
+    // 需添加权限<uses-permission android:name="android.permission.ACCESS_WIFI_STATE"/>
+
+    /**
+     * 获取MAC地址
+     */
+    public static String getMacAddress(Context context) {
+        String macAddress;
+        WifiManager wifi = (WifiManager) context
+                .getSystemService(Context.WIFI_SERVICE);
+        WifiInfo info = wifi.getConnectionInfo();
+        macAddress = info.getMacAddress();
+        if (null == macAddress) {
+            return "";
+        }
+        macAddress = macAddress.replace(":", "");
+        return macAddress;
+    }
+
+    /**
+     * 获取手机厂商，如Xiaomi
+     */
+    public static String getOsName() {
+        String MANUFACTURER = Build.MANUFACTURER;
+        return MANUFACTURER;
+    }
+
+    /**
+     * 获取手机型号，如MI2SC
+     */
+    public static String getModel() {
+        String model = android.os.Build.MODEL;
+        if (model != null) {
+            model = model.trim().replaceAll("\\s*", "");
+        } else {
+            model = "";
+        }
+        return model;
+    }
+
+    /**
+     * 跳转至拨号界面
+     */
+    public static void callDial(Context context, String phoneNumber) {
+        context.startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + phoneNumber)));
+    }
+
+    /**
+     * 拨打电话
+     */
+    public static void call(Context context, String phoneNumber) {
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(context, "没获取到权限", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        context.startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + phoneNumber)));
+    }
+
+}
